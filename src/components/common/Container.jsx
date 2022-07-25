e18204d1b5964df6d5d482ffacb6c7db9fc19080
DDD120 <dltnals9794@naver.com>
2022-07-25 21:40:12 +0900

    Feat 전체화면 기능 추가

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { AiOutlineFullscreenExit, AiOutlineFullscreen } from "react-icons/ai";
 
 const Layout = styled.div`
   min-height: 100%;
@@ -8,11 +11,38 @@ const Layout = styled.div`
   overflow: hidden;
 `;
 
+const FullscreenBtn = styled.button`
+  position: absolute;
+  background-color: #eeeeee;
+  padding: 4px;
+  font-size: 1.2rem;
+  top: 10px;
+  right: 10px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
 const Container = ({ children }) => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const handle = useFullScreenHandle();
+
+  const handFullscreen = () => {
+    isFullscreen ? handle.enter() : handle.exit();
+    setIsFullscreen((prev) => !prev);
+  };
+
   return (
-    <Layout>
-      <div>{children}</div>
-    </Layout>
+    <FullScreen handle={handle}>
+      <Layout>
+        <FullscreenBtn onClick={handFullscreen}>
+          {isFullscreen ? <AiOutlineFullscreenExit /> : <AiOutlineFullscreen />}
+        </FullscreenBtn>
+        {children}
+      </Layout>
+    </FullScreen>
   );
 };
 
